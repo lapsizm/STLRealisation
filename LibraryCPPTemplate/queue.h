5bba6ba9671c31a71d50b31e4f360fcb3c318e79
lapsizm <murygin_280702@mail.ru>
2021-12-23 21:34:07 +0300

    queue

--- LibraryCPPTemplate/queue.h
+++ LibraryCPPTemplate/queue.h
@@ -1,45 +1,76 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 
-template <typename Data> class Queue
-{
+#include "list.h"
+
+template<typename Data>
+class Queue {
+private:
+    // private data should be here
+    Data *queue;
+    int size;
+    int capacity;
+
 public:
     // Create empty queue
-    Queue()
-    {
+    Queue() : size(0), capacity(10) {
+        queue = new Data[capacity];
     }
 
     // Deletes queue
-    ~Queue()
-    {
+    ~Queue() {
+        delete queue;
+        size = 0;
+        capacity = 0;
     }
 
     // Includes new element into the queue
     // Should be O(1) on average
-    void insert(Data data)
-    {
+    void insert(Data data) {
+        if (size < capacity) {
+            queue[size] = data;
+            ++size;
+        } else {
+            capacity *= 2;
+            Data *new_queue = new Data[capacity];
+            for(int i = 0; i < size; ++i){
+                new_queue[i] = queue[i];
+            }
+            new_queue[size] = data;
+            delete[] queue;
+            queue = new_queue;
+            ++size;
+        }
     }
 
     // Retrieves first element from the queue
-    Data get() const
-    {
-        return Data();
+    Data get() const {
+        if(size == 0){
+            return 0;
+        }
+        Data data = queue[0];
+        return data;
     }
 
     // Removes first element from the queue
     // Should be O(1) on average
-    void remove()
-    {
+    void remove() {
+        if(size == 0) return;
+
+        Data* new_queue = new Data[size - 1];
+        --size;
+        for(int i = 0; i < size; ++i){
+            new_queue[i] = queue[i+1];
+        }
+        delete[] queue;
+        queue = new_queue;
     }
 
     // Returns true if the queue is empty
-    bool empty() const
-    {
-        return true;
+    bool empty() const {
+        return size > 0;
     }
 
-private:
-    // private data should be here
 };
 
 #endif
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/kirill/lapsizm/isogramma
+# Install script for directory: /home/kirill/lapsizm/iso2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/kirill/lapsizm/isogramma/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/kirill/lapsizm/iso2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
